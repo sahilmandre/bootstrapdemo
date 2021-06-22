9fb7305972a016ba5cd06cb41e76c6e554e53742
sahilmandre <sahilmandre@gmail.com>
2021-06-23 01:03:41 +0530

    completed

---
+++ Public/index.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoic2FoaWxtYW5kcmUiLCJhIjoiY2tvMW5jc3RtMG44OTJ2b2luMzd3NnNyeiJ9.g7jt2B76aGtMNxGvYN5m3A";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  center: [23.181647310451485, 79.94548421027008],
+  zoom: 18,
+});
